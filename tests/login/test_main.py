6c90651abd28b2e8d2ad058bdbb5aa74e93f3723
Nicolas Drebenstedt <drebenstedtn@rki.de>
2024-07-26 14:48:40 +0200

    debug jpgs

--- tests/login/test_main.py
+++ tests/login/test_main.py
@@ -1,3 +1,6 @@
+import logging
+from pathlib import Path
+
 import pytest
 from playwright.sync_api import Page, expect
 
@@ -13,7 +16,11 @@ def test_login(page: Page) -> None:
     page.get_by_placeholder("Username").fill(username)
     page.get_by_placeholder("Password").fill(password.get_secret_value())
     page.screenshot(path="test_login-login-screen.jpeg")
+    assert Path("test_login-login-screen.jpeg").exists()
+    logging.info(Path("test_login-login-screen.jpeg").as_posix())
 
     page.get_by_text("Log in").click()
     expect(page.get_by_test_id("search-container")).to_be_visible()
     page.screenshot(path="test_login-redirected-home.jpeg")
+    assert Path("test_login-redirected-home.jpeg").exists()
+    logging.info(Path("test_login-redirected-home.jpeg").as_posix())

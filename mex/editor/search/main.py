c30091a8d04dffd971f2999224b65f2365ea8b6d
Kamran Ali <AliK@rki.de>
2024-09-12 14:32:08 +0200

    implement simple search

--- mex/editor/search/main.py
+++ mex/editor/search/main.py
@@ -1,5 +1,6 @@
 import reflex as rx
 
+from mex.editor import styles
 from mex.editor.layout import page
 from mex.editor.search.models import SearchResult
 from mex.editor.search.state import SearchState
@@ -13,27 +14,105 @@ def search_result(result: SearchResult) -> rx.Component:
             rx.text(result.preview, weight="light"),
             href=f"/item/{result.identifier}",
         ),
-        style={"width": "80%"},
+        style={"width": "100%"},
+    )
+
+
+def sidebar() -> rx.Component:
+    """Render sidebar with a search input and checkboxes for filtering entity types."""
+    return rx.box(
+        rx.debounce_input(
+            rx.input(
+                rx.input.slot(rx.icon("search"), padding_left="0"),
+                placeholder="Search here...",
+                value=SearchState.query_string,
+                on_change=SearchState.set_query_string,
+                max_length=100,
+                style=styles.ghost_input_style,
+            ),
+            style={"margin": "1em 0 1em"},
+            debounce_timeout=250,
+        ),
+        rx.vstack(
+            rx.foreach(
+                SearchState.entity_types,
+                lambda choice: rx.debounce_input(
+                    rx.chakra.checkbox(
+                        choice[0],
+                        checked=choice[1],
+                        on_change=lambda val: SearchState.set_entity_type(
+                            value=val, index=choice[0]
+                        ),  # type: ignore
+                    ),
+                    debounce_timeout=100,
+                ),
+            ),
+            style={"margin": "2em 0"},
+        ),
+        width="20%",
+        height="100vh",
+        padding="20px",
+        style={"margin": "2em 0 0"},
+        bg=rx.color("gray", 2),
+    )
+
+
+def pagination() -> rx.Component:
+    """Render pagination for navigating search results."""
+    return rx.box(
+        rx.center(
+            rx.button(
+                rx.text("Previous", weight="bold"),
+                on_click=SearchState.go_to_previous_page,
+                disabled=SearchState.disable_previous_page,
+                spacing="2",
+                width="120px",
+                margin_right="10px",
+            ),
+            rx.select(
+                SearchState.total_pages,
+                value=f"{SearchState.current_page}",
+                on_change=SearchState.set_page,
+            ),
+            rx.button(
+                rx.text("Next", weight="bold"),
+                on_click=SearchState.go_to_next_page,
+                disabled=SearchState.disable_next_page,
+                spacing="2",
+                width="120px",
+                margin_left="10px",
+            ),
+        ),
+        width="100%",
+    )
+
+
+def main_content() -> rx.Component:
+    """Render the search results with a heading, result list, and pagination."""
+    return rx.box(
+        rx.heading(
+            f"showing {SearchState.current_results_length} "
+            f"of total {SearchState.total} items found",
+            custom_attrs={"data-testid": "search-heading"},
+            style={"margin": "1em 0"},
+            size="3",
+        ),
+        rx.vstack(
+            rx.foreach(
+                SearchState.results,
+                search_result,
+            ),
+            pagination(),
+            padding="10px",
+        ),
+        on_mount=SearchState.refresh,
+        style={"width": "100%", "margin": "2em 0 0"},
+        width="100%",
+        custom_attrs={"data-testid": "search-section"},
     )
 
 
 def index() -> rx.Component:
     """Return the index for the search component."""
-    return page(
-        rx.section(
-            rx.heading(
-                "Search",
-                custom_attrs={"data-testid": "search-heading"},
-                style={"margin": "1em 0"},
-            ),
-            rx.vstack(
-                rx.foreach(
-                    SearchState.results,
-                    search_result,
-                ),
-            ),
-            on_mount=SearchState.refresh,
-            style={"width": "100%"},
-            custom_attrs={"data-testid": "search-section"},
-        ),
-    )
+    # consider breadcrumb https://reflex.dev/docs/library/chakra/navigation/breadcrumb/
+    return page(sidebar(), main_content())
--- mex/editor/search/state.py
+++ mex/editor/search/state.py
@@ -1,8 +1,15 @@
+import math
+
 import reflex as rx
 from pydantic import BaseModel
+from requests import HTTPError
 
 from mex.common.backend_api.connector import BackendApiConnector
-from mex.common.models import AnyMergedModel
+from mex.common.logging import logger
+from mex.common.models import (
+    MERGED_MODEL_CLASSES_BY_NAME,
+    AnyMergedModel,
+)
 from mex.editor.search.models import SearchResult
 from mex.editor.transform import render_model_preview, render_model_title
 
@@ -17,6 +24,55 @@ class SearchState(rx.State):
 
     results: list[SearchResult] = []
     total: int = 0
+    query_string: str = ""
+    entity_types: dict[str, bool] = {k: False for k in MERGED_MODEL_CLASSES_BY_NAME}
+
+    current_page: int = 1
+    limit: int = 50
+
+    @rx.var
+    def disable_previous_page(self) -> bool:
+        """Disable the 'Previous' button if on the first page."""
+        return self.current_page <= 1
+
+    @rx.var
+    def disable_next_page(self) -> bool:
+        """Disable the 'Next' button if on the last page."""
+        max_page = math.ceil(self.total / self.limit)
+        return self.current_page >= max_page
+
+    @rx.var
+    def current_results_length(self) -> int:
+        """Return the number of current search results."""
+        return len(self.results)
+
+    @rx.var
+    def total_pages(self) -> list[str]:
+        """Return a list of total pages based on the number of results."""
+        return [f"{i+1}" for i in range(math.ceil(self.total / self.limit))]
+
+    def set_query_string(self, value: str):
+        """Set the query string and refresh the results."""
+        self.query_string = value
+        self.refresh()
+
+    def set_entity_type(self, value, index):
+        """Set the entity type for filtering and refresh the results."""
+        self.entity_types[index] = value
+        self.refresh()
+
+    def set_page(self, page_number: str | int):
+        """Set the current page and refresh the results."""
+        self.current_page = int(page_number)
+        self.refresh()
+
+    def go_to_previous_page(self):
+        """Navigate to the previous page."""
+        self.set_page(self.current_page - 1)
+
+    def go_to_next_page(self):
+        """Navigate to the next page."""
+        self.set_page(self.current_page + 1)
 
     def refresh(self) -> None:
         """Refresh the search results."""
@@ -24,7 +80,22 @@ class SearchState(rx.State):
         connector = BackendApiConnector.get()
 
         # TODO: use a specialized merged-item search method (stop-gap MX-1581)
-        response = connector.request("GET", "merged-item?limit=100")
+        try:
+            response = connector.request(
+                "GET",
+                "merged-item",
+                params={
+                    "q": self.query_string,
+                    "entityType": [k for k, v in self.entity_types.items() if v],  # type: ignore
+                    "skip": str(self.limit * (self.current_page - 1)),
+                    "limit": str(self.limit),
+                },
+            )
+        except HTTPError as exc:
+            self.results = []
+            self.total = 0
+            logger.error("backend error: ", exc.response.text)
+            return
         items = _BackendSearchResponse.model_validate(response).items
         self.results = [
             SearchResult(
@@ -35,3 +106,4 @@ class SearchState(rx.State):
             for item in items
         ]
         self.total = response["total"]
+        logger.info("results refreshed.")

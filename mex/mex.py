070ebc0271291d573fac9fcacbc541764c7b35a2
Kamran Ali <AliK@rki.de>
2024-09-16 15:30:22 +0200

    print settings on editor backend run

--- mex/mex.py
+++ mex/mex.py
@@ -2,6 +2,7 @@ import reflex as rx
 from reflex.components.radix import themes
 from reflex.utils.console import info as log_info
 
+from mex.common.logging import logger
 from mex.editor.api.main import check_system_status
 from mex.editor.edit.main import index as edit_index
 from mex.editor.login.main import index as login_index
@@ -46,6 +47,4 @@ app.register_lifespan_task(
     log_info,
     msg="MEx Editor is running, shut it down using CTRL+C",
 )
-app.register_lifespan_task(
-    EditorSettings.get,
-)
+app.register_lifespan_task(lambda: logger.info(EditorSettings.get().text()))

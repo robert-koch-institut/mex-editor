297bbe21b6d14a5bfaea21aaddb6199b36ab53b5
Kamran Ali <AliK@rki.de>
2024-09-17 10:06:42 +0200

    print settings on server startup

--- mex/mex.py
+++ mex/mex.py
@@ -43,8 +43,8 @@ app.api.add_api_route(
     check_system_status,
     tags=["system"],
 )
+app.register_lifespan_task(lambda: logger.info(EditorSettings.get().text()))
 app.register_lifespan_task(
     log_info,
     msg="MEx Editor is running, shut it down using CTRL+C",
 )
-app.register_lifespan_task(lambda: logger.info(EditorSettings.get().text()))

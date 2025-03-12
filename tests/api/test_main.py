f18a9f366b285bac460e17395b497f278d689b6f
Nicolas Drebenstedt <897972+cutoffthetop@users.noreply.github.com>
2025-03-12 10:29:59 +0100

    feature/mx-1767 show editor and backend versions (#275)
    ### PR Context
    I decided to show the versions as a tooltip to the logo. The logo
    component is used on the main page as well as the login, so you can also
    get to the version without logging in.
    
    ### Added
    - show mex-editor and mex-backend versions when hovering over logo

--- tests/api/test_main.py
+++ tests/api/test_main.py
@@ -1,7 +1,9 @@
 from fastapi.testclient import TestClient
 
+from mex.common.testing import Joker
+
 
 def test_health_check(client: TestClient) -> None:
     response = client.get("/_system/check")
     assert response.status_code == 200, response.text
-    assert response.json() == {"status": "ok"}
+    assert response.json() == {"status": "ok", "version": Joker()}

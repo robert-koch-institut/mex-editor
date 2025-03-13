194646928f85f698f8918763bd1032a892c02b9c
Nicolas Drebenstedt <897972+cutoffthetop@users.noreply.github.com>
2025-03-13 14:35:12 +0100

    feature/mx-1650 implement text and link editing (#259)
    ### PR Context
    
    - follow up to
    https://github.com/robert-koch-institut/mex-editor/pull/248
    
    ### Added
    
    - implement editing for text and link fields
    
    ---------
    
    Co-authored-by: RKI | Metadata Exchange <121876825+RKIMetadataExchange@users.noreply.github.com>

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -9,6 +9,8 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ### Added
 
+- implement editing for text and link fields
+
 ### Changes
 
 ### Deprecated
---
+++ tests/edit/test_state.py
@@ -0,0 +1,46 @@
+import pytest
+from reflex.state import serialize_mutable_proxy
+
+from mex.common.models import ContactPointRuleSetResponse, ExtractedContactPoint
+from mex.editor.edit.models import EditorPrimarySource, InputConfig
+from mex.editor.edit.state import EditState
+from mex.editor.edit.transform import transform_models_to_fields
+from mex.editor.models import EditorValue
+
+
+def test_state_get_primary_sources_by_field_name() -> None:
+    state = EditState()
+    rule_set = ContactPointRuleSetResponse(stableTargetId="someContactPoint")
+    extracted_item = ExtractedContactPoint(
+        email="test@foo.bar",
+        identifierInPrimarySource="fooBarContactPoint",
+        hadPrimarySource="somePrimarySource",
+    )
+    state.fields = transform_models_to_fields(
+        [extracted_item],
+        additive=rule_set.additive,
+        subtractive=rule_set.subtractive,
+        preventive=rule_set.preventive,
+    )
+
+    with pytest.raises(ValueError, match="field not found: someField"):
+        state._get_primary_sources_by_field_name("someField")
+
+    primary_sources = serialize_mutable_proxy(
+        state._get_primary_sources_by_field_name("email")
+    )
+
+    assert primary_sources == [
+        EditorPrimarySource(
+            name=EditorValue(text="somePrimarySource", href="/item/somePrimarySource"),
+            identifier="somePrimarySource",
+            input_config=None,
+            editor_values=[EditorValue(text="test@foo.bar")],
+        ),
+        EditorPrimarySource(
+            name=EditorValue(text="00000000000000", href="/item/00000000000000"),
+            identifier="00000000000000",
+            input_config=InputConfig(editable_text=True),
+            editor_values=[],
+        ),
+    ]

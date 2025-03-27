50e63c5558b66fe232868f351b17c91412a8352c
Zehra Ciftci <CiftciZ@rki.de>
2025-03-27 15:49:14 +0100

    rm external marker test

--- tests/aux_search/test_main.py
+++ tests/aux_search/test_main.py
@@ -20,8 +20,6 @@ def test_aux_navbar(aux_page: Page) -> None:
     nav_bar = page.get_by_test_id("aux-nav-bar")
     page.screenshot(path="tests_aux_search_test_main-test_aux_navbar.png")
     expect(nav_bar).to_be_visible()
-    nav_item = nav_bar.get_by_text("Wikidata")
-    expect(nav_item).to_be_visible()
 
 
 @pytest.mark.integration
@@ -33,15 +31,15 @@ def test_wikidata_search_and_import_results(aux_page: Page) -> None:
 
     # test search input is showing correctly
     search_input.fill("this doesn't exist gs871s9j91kksWN0shx345jnj")
-    expect(page.get_by_text("showing 0 of total 0 items found")).to_be_visible()
     page.screenshot(path="tests_aux_search_test_main-test_search-input-0-found.png")
+    expect(page.get_by_text("showing 0 of")).to_be_visible()
 
     # test expand button works
     search_input.fill("rki")
     expand_all_properties_button = page.get_by_test_id("expand-properties-button").nth(
         1
     )
-    expect(page.get_by_text("Robert Koch-Institut")).to_be_visible()
+    expect(page.get_by_text("Robert Koch-Institut").nth(1)).to_be_visible()
     expect(page.get_by_test_id("all-properties-display")).not_to_be_visible()
     expand_all_properties_button.click()
     expect(page.get_by_test_id("all-properties-display")).to_be_visible()
@@ -61,35 +59,38 @@ def test_wikidata_search_and_import_results(aux_page: Page) -> None:
 
 
 @pytest.mark.integration
-@pytest.mark.external
 def test_ldap_search_and_import_results(aux_page: Page) -> None:
     page = aux_page
-    page.get_by_text("LDAP").click()
+    ldap_tab = page.get_by_role("tab", name="LDAP")
+    ldap_tab.click()
     search_input = page.get_by_placeholder("Search here...")
     expect(search_input).to_be_visible()
 
     # test search input is showing correctly
-    search_input.fill("this doesn't exist gs871s9j91kksWN0shx345jnj")
-    expect(page.get_by_text("showing 0 of total 0 items found")).to_be_visible()
-    page.screenshot(path="tests_aux_search_test_main-test_search-input-0-found.png")
+    search_input.fill("doesn't exist gs871s9j91kksW*")
+    expect(page.get_by_text("Showing 0 of")).to_be_visible()
+    page.screenshot(
+        path="tests_aux_search_test_main-test_ldap_search-input-0-found.png"
+    )
 
     # test expand button works
     search_input.fill("Ciftci*")
     expand_all_properties_button = page.get_by_test_id("expand-properties-button").nth(
         1
     )
-    expect(page.get_by_text("Ciftci, Zehra")).to_be_visible()
+    page.screenshot(path="tests_aux_search_test_main-search_result.png")
+    expect(page.get_by_text("Ciftci")).to_be_visible()
     expect(page.get_by_test_id("all-properties-display")).not_to_be_visible()
     expand_all_properties_button.click()
     expect(page.get_by_test_id("all-properties-display")).to_be_visible()
-    page.screenshot(path="tests_aux_search_test_main-test_expand_button.png")
+    page.screenshot(path="tests_aux_search_test_main-test_ldap_expand_button.png")
 
     # test import button works
     import_button = page.get_by_text("Import").nth(1)
     import_button.click()
     expect(page.get_by_text("Aux search result imported successfully")).to_be_visible()
     expect(import_button).to_be_disabled()
-    page.screenshot(path="tests_aux_search_test_main-test_import_button.png")
+    page.screenshot(path="tests_aux_search_test_main-test_ldap_import_button.png")
 
     # test node was ingested into backend
     connector = BackendApiConnector.get()

6afbfeb928678467bd584db2f0a186414ac660bf
Henning Schiebenhöfer <SchiebenhoeferH@rki.de>
2025-06-24 14:07:06 +0200

    rename tests

--- tests/test_security.py
+++ tests/test_security.py
@@ -10,13 +10,13 @@ from mex.editor.security import (
 from mex.editor.settings import EditorSettings
 
 
-def test_has_write_access() -> None:
+def test_has_write_access_mex() -> None:
     assert has_write_access_mex("no_account", "who_dis?") is False
     assert has_write_access_mex("reader", "reader_pass") is False
     assert has_write_access_mex("writer", "writer_pass") is True
 
 
-def test_has_read_access() -> None:
+def test_has_read_access_mex() -> None:
     assert has_read_access_mex("no_account", "who_dis?") is False
     assert has_read_access_mex("reader", "reader_pass") is True
     assert has_read_access_mex("writer", "writer_pass") is True

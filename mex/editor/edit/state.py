e36ffacdd4da00d9ec204692e4e11e4ed6d144d5
eckelmannf <eckelmannf@rki.de>
2025-08-22 11:31:28 +0200

    Feature/mx 1959 toggle all switches (#400)
    ### PR Context
    <!-- Additional info for the reviewer -->
    
    ### Added
    <!-- New features and interfaces -->
    - added a switch to toggle all primary sources and values in the edit
    page
    
    ### Changes
    <!-- Changes in existing functionality -->
    
    ### Deprecated
    <!-- Soon-to-be removed features -->
    
    ### Removed
    <!-- Definitely removed features -->
    
    ### Fixed
    <!-- Fixed bugs -->
    
    ### Security
    <!-- Fixed vulnerabilities -->
    
    ---------
    
    Signed-off-by: eckelmannf <eckelmannf@rki.de>
    Signed-off-by: Nicolas Drebenstedt <897972+cutoffthetop@users.noreply.github.com>
    Co-authored-by: Nicolas Drebenstedt <897972+cutoffthetop@users.noreply.github.com>

--- mex/editor/edit/state.py
+++ mex/editor/edit/state.py
@@ -27,3 +27,25 @@ class EditState(RuleState):
             params = self.router.page.params.copy()
             params.pop("saved")
             yield self.push_url_params(params)
+
+    @rx.var
+    def any_primary_source_or_editor_value_enabled(self) -> bool:
+        """Determine if any primary source or editor value is enabled.
+
+        Returns:
+            bool: Return true if any primary source or editor value is enabled,
+            otherwise false.
+        """
+        return any(
+            ps.enabled or any(value.enabled for value in ps.editor_values)
+            for field in self.fields
+            for ps in field.primary_sources
+        )
+
+    def disable_all_primary_source_and_editor_values(self) -> None:
+        """Disable all primary source and editor values."""
+        for field in self.fields:
+            for ps in field.primary_sources:
+                ps.enabled = False
+                for value in ps.editor_values:
+                    value.enabled = False

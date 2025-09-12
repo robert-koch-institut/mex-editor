304fd7f411650b76922435ff82f4a4eec856aa5c
Nicolas Drebenstedt <drebenstedtn@rki.de>
2025-09-12 15:29:03 +0200

    expect over assert

--- tests/search/test_main.py
+++ tests/search/test_main.py
@@ -210,7 +210,7 @@ def test_reference_filter_fields_for_entity_type(
 
     dyn_tab = page.get_by_role("tab", name="Dynamic")
     dyn_tab.click()
-    assert page.get_by_test_id("reference-field-filter").is_visible()
+    expect(page.get_by_test_id("reference-field-filter")).to_be_visible()
 
     # select person entity
     entity_types = page.get_by_test_id("entity-types")

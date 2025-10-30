08fa5484945cb3a69ea419ffdd6aa7f9523cd456
Zehra Ciftci <CiftciZ@rki.de>
2025-10-30 15:08:49 +0100

    keyword fix

--- mex/editor/consent/main.py
+++ mex/editor/consent/main.py
@@ -98,12 +98,12 @@ def consent_box() -> rx.Component:
             rx.hstack(
                 rx.button(
                     "Einwilligen",
-                    on_click=ConsentState.submit_rule_set(consented="consent"),  # type: ignore[misc]
+                    on_click=ConsentState.submit_rule_set("consent"),  # type: ignore[misc]
                 ),
                 rx.spacer(),
                 rx.button(
                     "Ablehnen",
-                    on_click=ConsentState.submit_rule_set(consented="denial"),  # type: ignore[misc]
+                    on_click=ConsentState.submit_rule_set("denial"),  # type: ignore[misc]
                 ),
             ),
             style=rx.Style(

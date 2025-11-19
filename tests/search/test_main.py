31171a14699cf754d88f6db03eef078e9426e148
RKI | Metadata Exchange <121876825+RKIMetadataExchange@users.noreply.github.com>
2025-11-19 15:25:32 +0100

    Update neo4j Docker tag to v2025.09 (#458)
    This PR contains the following updates:
    
    | Package | Type | Update | Change | Pending |
    |---|---|---|---|---|
    | neo4j ([changelog](https://neo4j.com/release-notes/)) | service |
    minor | `2025.08-community` -> `2025.09-community` | `2025.10-community`
    |
    
    ---
    
    ### Configuration
    
    📅 **Schedule**: Branch creation - At any time (no schedule defined),
    Automerge - At any time (no schedule defined).
    
    🚦 **Automerge**: Disabled by config. Please merge this manually once you
    are satisfied.
    
    ♻ **Rebasing**: Whenever PR becomes conflicted, or you tick the
    rebase/retry checkbox.
    
    🔕 **Ignore**: Close this PR and you won't be reminded about this update
    again.
    
    ---
    
    - [ ] <!-- rebase-check -->If you want to rebase/retry this PR, check
    this box
    
    <!--renovate-debug:eyJjcmVhdGVkSW5WZXIiOiI0MS4xNjUuNSIsInVwZGF0ZWRJblZlciI6IjQyLjE2LjEiLCJ0YXJnZXRCcmFuY2giOiJtYWluIiwibGFiZWxzIjpbXX0=-->
    
    ---------
    
    Signed-off-by: eckelmannf <eckelmannf@rki.de>
    Signed-off-by: Nicolas Drebenstedt <897972+cutoffthetop@users.noreply.github.com>
    Co-authored-by: eckelmannf <eckelmannf@rki.de>
    Co-authored-by: Nicolas Drebenstedt <897972+cutoffthetop@users.noreply.github.com>
    Co-authored-by: Nicolas Drebenstedt <drebenstedtn@rki.de>

--- tests/search/test_main.py
+++ tests/search/test_main.py
@@ -253,14 +253,62 @@ def test_reference_filter_fields_for_entity_type(
     )
 
     expected_person_fields = [
-        "memberOf",
-        "affiliation",
-        "hadPrimarySource",
-        "stableTargetId",
+        "Fachgebiet",
+        "Affiliation",
+        "Primärsystem",
+        "Stabiler Identifikator",
     ]
     for field in expected_person_fields:
-        select_item = page.get_by_role("option", name=field)
+        select_item = page.get_by_role("option", name=field, exact=True)
         expect(select_item).to_be_visible()
+
+
+@pytest.mark.parametrize(
+    ("locale_id", "expected_items"),
+    [
+        pytest.param(
+            "de-DE",
+            [
+                "Affiliation",
+                "Autor*in",
+                "Datei",
+                "Fachgebiet",
+                "Kontakt",
+                "Nachfolge von",
+            ],
+            id="de-DE",
+        ),
+        pytest.param(
+            "en-US",
+            [
+                "Affiliation",
+                "Access platform",
+                "Contact",
+                "Author",
+                "Files",
+                "Publisher",
+            ],
+            id="en-US",
+        ),
+    ],
+)
+@pytest.mark.integration
+def test_reference_filter_field_translation(
+    frontend_url: str, writer_user_page: Page, locale_id: str, expected_items: list[str]
+) -> None:
+    page = writer_user_page
+    page.goto(frontend_url)
+    page.wait_for_selector("[data-testid='page-body']")
+
+    # switch language to specifid locale
+    lang_switcher = page.get_by_test_id("language-switcher")
+    lang_switcher.click()
+    page.get_by_test_id(f"language-switcher-menu-item-{locale_id}").click()
+
+    # check expected items existing
+    page.get_by_test_id("reference-field-filter-field").click()
+    for item in expected_items:
+        expect(page.get_by_role("option", name=item, exact=True)).to_be_visible()
 
 
 @pytest.mark.integration
@@ -278,7 +326,7 @@ def test_reference_filter(
     # open select
     page.get_by_test_id("reference-field-filter-field").click()
     # click concat option
-    page.get_by_role("option", name="contact").click()
+    page.get_by_role("option", name="Kontakt").click()
     # add invalid field
     page.get_by_test_id("reference-field-filter-add-id").click()
     page.get_by_test_id("reference-field-filter-id-0").fill("invalidIdentifier!")
@@ -315,7 +363,7 @@ def test_push_search_params(
 
     # load page and verify url
     page.goto(frontend_url)
-    page.wait_for_url("**/", timeout=10000)
+    page.wait_for_url("**/")
 
     # select an entity type
     entity_types = page.get_by_test_id("entity-types")
@@ -350,10 +398,10 @@ def test_push_search_params(
     page.screenshot(path="tests_search_test_main-test_push_search_params-on-load-2.png")
     primary_sources.get_by_text(primary_source.title[0].value).click()
     checked = primary_sources.get_by_role("checkbox", checked=True)
+    expect(checked).to_have_count(1)
     page.screenshot(
         path="tests_search_test_main-test_push_search_params-on-click-2.png"
     )
-    expect(checked).to_have_count(1)
 
     # expect parameter change to be reflected in url
     page.wait_for_url(

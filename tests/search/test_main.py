b301ca0e58377e9ae54967605670f0832ff4db50
Nicolas Drebenstedt <drebenstedtn@rki.de>
2025-11-19 16:07:04 +0100

    fix test

--- tests/search/test_main.py
+++ tests/search/test_main.py
@@ -396,12 +396,14 @@ def test_push_search_params(
     primary_sources = page.get_by_test_id("had-primary-sources")
     expect(primary_sources).to_be_visible()
     page.screenshot(path="tests_search_test_main-test_push_search_params-on-load-2.png")
-    primary_sources.get_by_text(primary_source.title[0].value).click()
+    checkbox = primary_sources.get_by_text(primary_source.title[0].value)
+    expect(checkbox).to_be_visible()
+    checkbox.click()
     page.screenshot(
         path="tests_search_test_main-test_push_search_params-on-click-2.png"
     )
     checked = primary_sources.get_by_role("checkbox", checked=True)
-    expect(checked).to_have_count(1, timeout=30_000)
+    expect(checked).to_have_count(1)
 
     # expect parameter change to be reflected in url
     page.wait_for_url(

a009cee9a42a10569f2802c90a8b7ca0ef15553d
Nicolas Drebenstedt <drebenstedtn@rki.de>
2026-02-09 18:45:58 +0100

    fix expect(nav_item).to_have_class

--- tests/edit/test_main.py
+++ tests/edit/test_main.py
@@ -42,7 +42,7 @@ def test_edit_page_updates_nav_bar(edit_page: Page) -> None:
     page.screenshot(path="tests_edit_test_main-test_edit_page_updates_nav_bar.png")
     nav_item = nav_bar.locator(".nav-item").all()[2]
     expect(nav_item).to_have_attribute("data-testid", "nav-item-/item/[item_id]")
-    expect(nav_item).to_have_class("nav-item-active")
+    expect(nav_item).to_have_class(re.compile(r"(^|\\s)nav-item-active(\\s|$)"))
 
 
 @pytest.mark.integration

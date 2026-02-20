00ab8430d112dafc57963ce35e450c5629ecd496
Nicolas Drebenstedt <drebenstedtn@rki.de>
2026-02-20 13:14:13 +0100

    fix tests

--- tests/merge/test_main.py
+++ tests/merge/test_main.py
@@ -38,6 +38,7 @@ def test_index(merge_page: Page) -> None:
 
 @pytest.mark.integration
 @pytest.mark.usefixtures("load_dummy_data")
+@pytest.mark.flaky(retries=3, delay=1)
 def test_search_input_merged(merge_page: Page) -> None:
     page = merge_page
 
@@ -216,8 +217,8 @@ def test_additional_titles_badge(
     # expect title is visible and there are additional titles for 'r2'
     expect(resource_r2_result).to_contain_text(first_title.value)
     additional_title_badge = page.get_by_test_id("additional-titles-badge").first
-    additional_title_badge.scroll_into_view_if_needed()
     expect(additional_title_badge).to_be_visible()
+    additional_title_badge.scroll_into_view_if_needed()
     page.screenshot(path="tests_search_test_additional_titles_badge_on_visible.png")
     expect(additional_title_badge).to_have_text(
         build_ui_label_regex("components.titles.additional_titles")

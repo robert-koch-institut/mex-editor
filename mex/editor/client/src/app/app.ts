5108426dedd4c6708f5d030e1add142448a3979f
eckelmannf <eckelmannf@rki.de>
2026-07-02 11:38:11 +0200

    renovate/angular22 (#78)
    ### PR Context
    had to change eslint rule for angular due to new changedetection
    strategy default. might me changed when eslint-angular gets an update
    https://github.com/angular-eslint/angular-eslint/blob/main/packages/eslint-plugin/docs/rules/prefer-on-push-component-change-detection.md#rationale
    
    ### Changes
    - updated to angular 22
    
    ---------
    
    Signed-off-by: eckelmannf <eckelmannf@rki.de>

--- mex/editor/client/src/app/app.ts
+++ mex/editor/client/src/app/app.ts
@@ -1,19 +1,13 @@
-import { Component } from "@angular/core";
+import { Component, ChangeDetectionStrategy } from "@angular/core";
 import { MatIcon } from "@angular/material/icon";
 import { RouterOutlet } from "@angular/router";
 import { NavBarComponent } from "./components/nav-bar-component/nav-bar-component";
 
-
 @Component({
   selector: "app-root",
-  imports: [
-    RouterOutlet,
-    MatIcon,
-    NavBarComponent
-  ],
+  imports: [RouterOutlet, MatIcon, NavBarComponent],
   templateUrl: "./app.html",
+  changeDetection: ChangeDetectionStrategy.Eager,
   styleUrl: "./app.scss",
 })
-export class App {
-
-}
+export class App {}

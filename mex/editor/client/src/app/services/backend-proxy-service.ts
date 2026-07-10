8c956c471668adb5cb129213843bcc5e0ae4806e
Fabian Eckelmann <EckelmannF@rki.de>
2026-07-10 09:45:29 +0200

    pr changes

--- mex/editor/client/src/app/services/backend-proxy-service.ts
+++ mex/editor/client/src/app/services/backend-proxy-service.ts
@@ -11,10 +11,10 @@ export interface PreviewItem {
 }
 
 /**
- * Model for mex.backend PaginatedPreviewItems.
+ * Model for mex.backend PaginatedItemContainer.
  */
-export interface PaginatedPreviewItems {
-  items: PreviewItem[];
+export interface PaginatedItemContainer<T> {
+  items: T[];
   total: number;
 }
 
@@ -31,7 +31,7 @@ export class BackendProxyService {
    * Get PreviewItems by its corrosponding endpoint.
    * @returns PreviewItems with pagination metadata.
    */
-  getPreviewItems(): Observable<PaginatedPreviewItems> {
-    return this._http.get<PaginatedPreviewItems>("api/v0/backend/preview-item");
+  getPreviewItems(): Observable<PaginatedItemContainer<PreviewItem>> {
+    return this._http.get<PaginatedItemContainer<PreviewItem>>("api/v0/backend/preview-item");
   }
 }

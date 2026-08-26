51fbdf81e9c0e714f42d41e6caf6491ac7b8b73d
Fabian Eckelmann <EckelmannF@rki.de>
2026-08-26 11:45:17 +0200

    correct translations for nested object properties and tests for field category pipe

--- mex/editor/client/src/app/shared/field-category-pipe.spec.ts
+++ mex/editor/client/src/app/shared/field-category-pipe.spec.ts
@@ -1,8 +1,30 @@
+import z from "zod";
+
 import { FieldCategoryPipe } from "./field-category-pipe";
 
 describe("FieldCategoryPipe", () => {
   it("create an instance", () => {
     const pipe = new FieldCategoryPipe();
     expect(pipe).toBeTruthy();
+  });
+
+  it("maps string types correctly", () => {
+    const pipe = new FieldCategoryPipe();
+    expect(pipe.transform(z.string())).toBe("optional");
+    expect(pipe.transform(z.string().nonempty())).toBe("required");
+    expect(pipe.transform(z.string().min(1))).toBe("required");
+    expect(pipe.transform(z.string().optional())).toBe("optional");
+    expect(pipe.transform(z.string().nullable())).toBe("optional");
+    expect(pipe.transform(z.string().nullish())).toBe("optional");
+  });
+
+  it("maps array types correctly", () => {
+    const pipe = new FieldCategoryPipe();
+    expect(pipe.transform(z.string().array())).toBe("optional");
+    expect(pipe.transform(z.string().array().nonempty())).toBe("required");
+    expect(pipe.transform(z.string().array().min(1))).toBe("required");
+    expect(pipe.transform(z.string().array().optional())).toBe("optional");
+    expect(pipe.transform(z.string().array().nullable())).toBe("optional");
+    expect(pipe.transform(z.string().array().nullish())).toBe("optional");
   });
 });
